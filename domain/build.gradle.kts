c310fe99565698e12bd26c002f239470c782fadc
alexey-va <freedeeml@gmail.com>
2026-09-25 21:05:06 +0300

    build: update arc-core to 2.7.13

--- domain/build.gradle.kts
+++ domain/build.gradle.kts
@@ -3,5 +3,5 @@ plugins {
 }
 
 dependencies {
-    implementation("ru.ruscrafting.arc:arc-core:2.7.9")
+    implementation("ru.ruscrafting.arc:arc-core:2.7.13")
 }
--- network-redis/build.gradle.kts
+++ network-redis/build.gradle.kts
@@ -4,11 +4,11 @@ plugins {
 
 dependencies {
     implementation(project(":domain"))
-    implementation("ru.ruscrafting.arc:arc-core:2.7.9")
-    implementation("ru.ruscrafting.arc:arc-core-redis:2.7.9")
+    implementation("ru.ruscrafting.arc:arc-core:2.7.13")
+    implementation("ru.ruscrafting.arc:arc-core-redis:2.7.13")
     implementation("com.google.code.gson:gson:2.11.0")
     implementation("org.slf4j:slf4j-api:2.0.16")
-    testImplementation("ru.ruscrafting.arc:arc-core-testing:2.7.9")
+    testImplementation("ru.ruscrafting.arc:arc-core-testing:2.7.13")
 }
 
 val integrationTest by sourceSets.creating {
@@ -20,7 +20,7 @@ configurations[integrationTest.implementationConfigurationName].extendsFrom(conf
 configurations[integrationTest.runtimeOnlyConfigurationName].extendsFrom(configurations.testRuntimeOnly.get())
 
 dependencies {
-    add(integrationTest.implementationConfigurationName, "ru.ruscrafting.arc:arc-core-integration-testing:2.7.9")
+    add(integrationTest.implementationConfigurationName, "ru.ruscrafting.arc:arc-core-integration-testing:2.7.13")
 }
 
 tasks.register<Test>("integrationTest") {
